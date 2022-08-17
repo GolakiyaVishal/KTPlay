e96c26a4cd067b3d5e8ff15f42eb1cc17189980d
goVishal <golakiyavishal812@gmail.com>
2022-08-17 15:33:15 +0530

    add sealed class

--- src/main/kotlin/SealedTest.kt
+++ src/main/kotlin/SealedTest.kt
@@ -1,2 +1,35 @@
-class SealedTest {
+/**
+ *  Sealed class is Abstract by self
+ *  hold abstract member
+ *  constructors are by default protected
+ *      we can make it private
+ *
+ *  Sealed class ensure the type safety, it match the type at compile-time
+ *
+ * */
+
+sealed class Error {
+    constructor() // protected
+    private constructor(i: Int)
+
+}
+
+class InternalError: Error() {
+
+}
+
+class JsonError: Error() {
+
+}
+
+fun checkError(t: Error) {
+    when(t) {
+        is InternalError -> { println("Internal Error") }
+        is JsonError -> { println("Json Error") }
+        else -> {println("no Error")}
+    }
+}
+
+fun main() {
+    checkError(JsonError())
 }
